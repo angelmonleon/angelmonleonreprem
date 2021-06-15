a7e7ac8b61b51be685df572cecb582c75de99f3a
1DAW <1DAW@106alum08>
2021-06-15 13:09:40 +0200

    añadimos despedida

--- angelmonleon/src/angelmonleon/despedida.java
+++ angelmonleon/src/angelmonleon/despedida.java
@@ -2,6 +2,6 @@ package angelmonleon;
 
 public class despedida {
 public static void main(String[] args) {
-System.out.print("Ángel Monleón se despide");
+System.out.print("Ángel Monleón se despide ");
 }
 }
